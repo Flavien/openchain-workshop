8e00b71367de74279d1a9337418c9567ed99f28f
Flavien Charlon <flavien@charlon.net>
2016-04-11 11:16:01 +0100

    Complete the workshop

--- workshop.js
+++ workshop.js
@@ -8,10 +8,10 @@ var privateKey = bitcore.HDPrivateKey.fromSeed(seed, "openchain");
 var address = privateKey.publicKey.toAddress();
  
 // Calculate the accounts corresponding to the private key
-var sourceAssetPath = "?";
-var issuancePath = "?";
+var sourceAssetPath = "/asset/p2pkh/XvFZfScne2h1KkWjuqTjZzzKEcaFbGWx4d/";
+var issuancePath = "/asset/p2pkh/" + address + "/";
 var assetPath = issuancePath;
-var fromPath = "?";
+var fromPath = "/p2pkh/" + address + "/";
 var storagePath = "/storage/";
 var issuedPath = "/issued/";
 
@@ -25,4 +25,25 @@ var signer = new openchain.MutationSigner(privateKey);
 // Initialize the client 
 client.initialize().then(function () {
     
+    client.getAccountRecord(fromPath, sourceAssetPath).then(function(result) {
+        var amount = result.balance;
+        console.log("Balance: " + amount.toString());
+        
+        return new openchain.TransactionBuilder(client)
+            // Add the key to the transaction builder 
+            .addSigningKey(signer)
+            // Add some metadata to the transaction 
+            .setMetadata({ "memo": "Coinsilium workshop!" })
+            .updateAccountRecord(fromPath, sourceAssetPath, -amount)
+            .then(function (transactionBuilder) {
+                return transactionBuilder.updateAccountRecord(storagePath, sourceAssetPath, amount);
+            }).then (function (transactionBuilder) {
+                return transactionBuilder.updateAccountRecord(issuancePath, assetPath, -amount);
+            }).then (function (transactionBuilder) {
+                return transactionBuilder.updateAccountRecord(issuedPath, assetPath, amount);
+            }).then (function (transactionBuilder) {
+                return transactionBuilder.submit();
+            })
+            .then(function (result) { console.log(result); }, function (result) { console.error(result); }); 
+    });
 });
